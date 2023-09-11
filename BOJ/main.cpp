ad6b53130ca4b0260d620e8347dbfb730ec344b5
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-09-11 16:12:35 +0900

    Initial Commit

---
+++ BOJ/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BOJ
+//
+//  Created by 한재혁 on 2023/09/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
