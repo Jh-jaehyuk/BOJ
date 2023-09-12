aa9b2ce1e5e18eff2f379505da1f73c76f462512
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-09-12 16:04:44 +0900

    https://www.acmicpc.net/problem/23736

---
+++ BOJ/20000++/23736.cpp
@@ -0,0 +1,36 @@
+//
+//  23736.cpp
+//  BOJ
+//
+//  Created by 한재혁 on 2023/09/11.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int A, B, K, X;
+    cin >> A >> B >> K >> X;
+
+    int result = 0;
+
+    for (int i = A; i <= B; i++) {
+        if (abs(K - i) <= X) {
+            result++;
+        }
+    }
+
+    if (result == 0) {
+        cout << "IMPOSSIBLE" << "\n";
+    }
+
+    else {
+        cout << result << "\n";
+    }
+    
+    return 0;
+}
