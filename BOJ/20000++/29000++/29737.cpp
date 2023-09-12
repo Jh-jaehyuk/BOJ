0c274ad068cc94972b4c63b7bfd0c8e58deb6d8b
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-09-12 19:33:48 +0900

    https://www.acmicpc.net/problem/29737 브실이는 잔디가 좋아🌱

--- BOJ/20000++/29000++/29737.cpp
+++ BOJ/20000++/29000++/29737.cpp
@@ -11,13 +11,12 @@ using namespace std;
 
 struct Friend {
     string name;
-    int m;
-    int c;
-    int d;
-    int f;
+    int len, freeze, day, fail;
 };
 
-static vector<Friend> V;
+static vector<Friend> A;
+
+bool cmp(Friend i, Friend j);
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -26,76 +25,128 @@ int main() {
     int N, W;
     cin >> N >> W;
     
-    for (int n = 0; n < N; n++) {
+    for (int t = 0; t < N; t++) {
         string s;
         cin >> s;
         
-        Friend tmp;
-        tmp.name = s;
-        tmp.m = 0;
-        tmp.c = 0;
-        tmp.d = 0;
-        tmp.f = 0;
-        
-        char tmp_board[7][102];
-        char board[102][7];
+        string board = "";
         
         for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < W; j++) {
-                cin >> tmp_board[i][j];
-            }
+            string tmp;
+            cin >> tmp;
+            board += tmp;
         }
+        
+        int Max_L, Max_F, Max_D, L, fail_count, F, D, cnt, f;
+        Max_L = Max_F = Max_D = L = fail_count = F = D = cnt = f = 0;
         
         for (int j = 0; j < W; j++) {
             for (int i = 0; i < 7; i++) {
-                board[j][i] = tmp_board[i][j];
+                if (board[W * i + j] == 'O') {
+                    if (L == 0) {
+                        L++;
+                        D = cnt;
+                    }
+                    
+                    else {
+                        L++;
+                        F += f;
+                        f = 0;
+                    }
+                }
+                
+                else if (board[W * i + j] == 'F') {
+                    if (L != 0) {
+                        f++;
+                    }
+                }
+                
+                else if (board[W * i + j] == 'X') {
+                    if (L > Max_L) {
+                        Max_L = L;
+                        Max_D = D;
+                        Max_F = F;
+                    }
+                    
+                    else if (L == Max_L) {
+                        if (Max_F > F) {
+                            Max_F = F;
+                            Max_D = D;
+                        }
+                    }
+                    
+                    L = F = D = f = 0;
+                    fail_count++;
+                }
+                cnt++;
+            }
+        }
+        if (L > Max_L) {
+            Max_L = L;
+            Max_D = D;
+            Max_F = F;
+        }
+        
+        else if (L == Max_L) {
+            if (Max_F > F) {
+                Max_F = F;
+                Max_D = D;
             }
         }
         
-        int Max = 0;
-        int Day = 0;
-        int Count = 0;
-        int Fail = 0;
+        Friend temp;
+        temp.name = s;
+        temp.freeze = Max_F;
+        temp.len = Max_L;
+        temp.fail = fail_count;
+        temp.day = Max_D;
         
-        int cnt = 0;
-        int f_cnt = 0;
+        A.push_back(temp);
+    }
+    
+    sort(A.begin(), A.end(), cmp);
+    
+    int idx = 1;
+    Friend prev = A[0];
+    
+    for (int a = 0; a < N; a++) {
+        Friend i = A[a];
         
-        for (int i = 0; i < W; i++) {
-            for (int j = 0; j < 7; j++) {
-                if (board[i][j] == 'O') {
-                    cnt++;
-                    Max = max(Max, cnt);
-                }
-                
-                else if (board[i][j] == 'F') {
-                    f_cnt++;
+        if (a != 0 && (i.len != prev.len || i.freeze != prev.freeze || i.day != prev.day || i.fail != prev.fail)) {
+            idx++;
+        }
+        
+        prev = i;
+        cout << idx << ". " << i.name << "\n";
+    }
+    
+    return 0;
+}
+
+bool cmp(Friend i, Friend j) {
+    if (i.len != j.len) {
+        return i.len > j.len;
+    }
+    
+    else {
+        if (i.freeze != j.freeze) {
+            return i.freeze < j.freeze;
+        }
+        
+        else {
+            if (i.day != j.day) {
+                return i.day < j.day;
+            }
+            
+            else {
+                if (i.fail != j.fail) {
+                    return i.fail < j.fail;
                 }
                 
                 else {
-                    if (cnt == Max) {
-                        Day = i + j + 1 - cnt;
-                        Count = f_cnt;
-                    }
-                    
-                    cnt = 0;
-                    f_cnt = 0;
-                    Fail++;
+                    return i.name < j.name;
                 }
             }
         }
-        
-        if (cnt > Max) {
-            Max = cnt;
-        }
-        
-        tmp.m = Max;
-        tmp.d = Day;
-        tmp.c = Count;
-        
-        
-        V.push_back(tmp);
     }
-    
-    
-    return 0;
 }
