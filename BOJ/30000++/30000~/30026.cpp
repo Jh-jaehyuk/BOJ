61c20bcb67c4d219fb04e6f9b8b45b010df47003
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-09-19 17:54:47 +0900

    NOT SOLVED! Change solution.

--- BOJ/30000++/30000~/30026.cpp
+++ BOJ/30000++/30000~/30026.cpp
@@ -9,10 +9,14 @@
 
 using namespace std;
 
-static vector<int> edges;
+static int N;
+static vector<int> A;
 static vector<int> parent;
+static vector<bool> visited;
+static vector<vector<int>> cycle_decomposition;
 static vector<pair<int, int>> result;
 
+void DFS(int node, int count, int start);
 int find_parent(int x);
 void union_parent(int a, int b);
 
@@ -20,40 +24,67 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     
-    int N;
     cin >> N;
     
-    edges.resize(N + 1);
+    A.resize(N + 1);
     parent.resize(N + 1);
+    visited.resize(N + 1);
+    cycle_decomposition.resize(N + 1);
+    
+    fill(visited.begin(), visited.end(), false);
     
     for (int i = 1; i <= N; i++) {
-        cin >> edges[i];
-        parent[edges[i]] = i;
+        cin >> A[i];
+        parent[i] = i;
     }
+    
+    int count = 0;
+    for (int i = 1; i <= N; i++) {
+        if (!visited[i]) {
+            DFS(i, count, i);
+            count++;
+        }
+    }
+    
+    if (count == 1) {
+        cout << 0 << " " << 0 << "\n";
+        return 0;
+    }
+    
+    int min_mana = 0;
+    int use_magic = 0;
     
     int prev = 1;
-    int count = 0;
-    int mana = 0;
-    
     for (int i = 2; i <= N; i++) {
         if (find_parent(prev) != find_parent(i)) {
-            count++;
-            mana += (i - prev) * (i - prev);
             union_parent(prev, i);
-            result.push_back(make_pair(prev, i));
-            prev = i;
+            min_mana += (i - prev) * (i - prev);
+            use_magic++;
+            result.push_back(make_pair(A[prev], A[i]));
         }
+        prev = i;
     }
     
-    cout << count << " " << mana << "\n";
+    cout << use_magic << " " << min_mana << "\n";
     
-    if (!result.empty()) {
-        for (pair<int, int> i : result) {
-            cout << i.first << " " << i.second << "\n";
-        }
+    for (auto i : result) {
+        cout << i.first << " " << i.second << "\n";
     }
     
     return 0;
+}
+
+void DFS(int node, int count, int start) {
+    visited[node] = true;
+    cycle_decomposition[count].push_back(node);
+    
+    if (find_parent(start) != find_parent(node)) {
+        union_parent(start, node);
+    }
+    
+    if (!visited[A[node]]) {
+        DFS(A[node], count, start);
+    }
 }
 
 int find_parent(int x) {
