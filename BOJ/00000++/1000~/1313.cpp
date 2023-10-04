fb59e47cbf0203d8b14636e1e5fb68968547a9e5
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-10-04 16:06:34 +0900

    https://www.acmicpc.net/problem/1313 합성소수

---
+++ BOJ/00000++/1000~/1313.cpp
@@ -0,0 +1,8 @@
+//
+//  1313.cpp
+//  BOJ
+//
+//  Created by J213h on 2023/10/04.
+//
+
+#include <stdio.h>
---
+++ BOJ/00000++/1000~/1313_hardcode.cpp
@@ -0,0 +1,69 @@
+//
+//  1313.cpp
+//  BOJ
+//
+//  Created by J213h on 2023/10/04.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static int prime[1000001];
+bool isPrime(int x);
+bool Check(string s);
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    for (int i = 2; i <= 1000000; i++) {
+        if (isPrime(i)) {
+            prime[i] = 1;
+        }
+    }
+    
+    for (int i = 100; i <= 1000000; i++) {
+        if (!prime[i] && Check(to_string(i))) {
+            cout << i << "\n";
+        }
+    }
+    
+    return 0;
+}
+
+bool isPrime(int x) {
+    int sq = (int)sqrt(x);
+    for (int i = 2; i <= sq; i++) {
+        if (x % i == 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+bool Check(string s) {
+    int ss = (int)s.size();
+    
+    bool flag = false;
+    for (int i = 2; i < ss; i++) {
+        for (int j = 0; j <= ss - i; j++) {
+            int tmp = stoi(s.substr(j, i));
+            if (!prime[tmp]) {
+                flag = true;
+                break;
+            }
+        }
+        
+        if (flag) {
+            break;
+        }
+    }
+    
+    if (!flag) {
+        return true;
+    }
+    
+    return false;
+}
