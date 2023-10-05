713360ef07add2d74f9b34de839729280c896294
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-10-05 20:10:46 +0900

    https://www.acmicpc.net/problem/17142 & https://www.acmicpc.net/problem/4991

--- BOJ/00000++/4000~/4991.cpp
+++ BOJ/00000++/4000~/4991.cpp
@@ -5,4 +5,136 @@
 //  Created by J213h on 2023/10/05.
 //
 
-#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static int N, M;
+static char board[21][21];
+static vector<pair<int, int>> V;
+static int visited[21][21][21][21];
+
+static int dx[4] = { -1, 1, 0, 0 };
+static int dy[4] = { 0, 0, -1, 1 };
+static int result;
+
+void Solve();
+void Swap(pair<int, int>& a, pair<int, int>& b);
+void DFS(vector<pair<int, int>>& A, int depth, int count);
+void BFS(int sx, int sy);
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    while (true) {
+        cin >> M >> N;
+        
+        if (N == 0 && M == 0) {
+            break;
+        }
+        
+        Solve();
+    }
+    
+    return 0;
+}
+
+void Solve() {
+    V.clear();
+    fill(&visited[0][0][0][0], &visited[20][20][20][21], -1);
+    result = (int)1e9;
+    
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> board[i][j];
+            
+            if (board[i][j] == '*') {
+                V.push_back(make_pair(i, j));
+                count++;
+            }
+            
+            if (board[i][j] == 'o') {
+                V.insert(V.begin(), make_pair(i, j));
+                board[i][j] = '.';
+            }
+        }
+    }
+    
+    for (pair<int, int> i : V) {
+        BFS(i.first, i.second);
+    }
+    
+    DFS(V, 1, count + 1);
+    cout << (result == (int)1e9 ? -1 : result) << "\n";
+}
+
+void Swap(pair<int, int>& a, pair<int, int>& b) {
+    pair<int, int> tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void DFS(vector<pair<int, int>>& A, int depth, int count) {
+    if (depth == count) {
+        int tmp = 0;
+        
+        for (int i = 0; i < count - 1; i++) {
+            int d = visited[A[i].first][A[i].second][A[i + 1].first][A[i + 1].second];
+            
+            if (d == -1) {
+                return;
+            }
+            
+            else {
+                tmp += d;
+            }
+        }
+        
+        result = min(result, tmp);
+        return;
+    }
+    
+    for (int i = depth; i < count; i++) {
+        Swap(A[depth], A[i]);
+        DFS(A, depth + 1, count);
+        Swap(A[depth], A[i]);
+    }
+}
+
+void BFS(int sx, int sy) {
+    int dist[21][21];
+    fill(&dist[0][0], &dist[20][21], -1);
+    queue<pair<int, int>> myqueue;
+    myqueue.push(make_pair(sx, sy));
+    dist[sx][sy] = 0;
+    
+    while (!myqueue.empty()) {
+        int x = myqueue.front().first;
+        int y = myqueue.front().second;
+        myqueue.pop();
+        
+        visited[sx][sy][x][y] = dist[x][y];
+        
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+                continue;
+            }
+            
+            if (board[nx][ny] == 'x') {
+                continue;
+            }
+            
+            if (dist[nx][ny] != -1) {
+                continue;
+            }
+            
+            dist[nx][ny] = dist[x][y] + 1;
+            myqueue.push(make_pair(nx, ny));
+        }
+    }
+}
--- BOJ/10000++/17000~/17142.cpp
+++ BOJ/10000++/17000~/17142.cpp
@@ -5,4 +5,115 @@
 //  Created by J213h on 2023/10/05.
 //
 
-#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static int N, M;
+static int board[51][51];
+static int dx[4] = { -1, 1, 0, 0 };
+static int dy[4] = { 0, 0, -1, 1 };
+static int check[10];
+static vector<pair<int, int>> virus;
+static int visited[51][51];
+static int result = (int)1e9;
+
+void DFS(int idx, int count);
+void BFS(queue<pair<int, int>> myqueue);
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cin >> N >> M;
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> board[i][j];
+            
+            if (board[i][j] == 2) {
+                virus.push_back(make_pair(i, j));
+            }
+        }
+    }
+    
+    DFS(0, 0);
+    cout << (result == (int)1e9 ? -1 : result) << "\n";
+    
+    return 0;
+}
+
+void DFS(int idx, int count) {
+    if (count == M) {
+        fill(&visited[0][0], &visited[50][51], -1);
+        queue<pair<int, int>> myqueue;
+        
+        for (int i = 0; i < virus.size(); i++) {
+            if (check[i]) {
+                myqueue.push(make_pair(virus[i].first, virus[i].second));
+                visited[virus[i].first][virus[i].second] = 0;
+            }
+        }
+        
+        BFS(myqueue);
+        return;
+    }
+    
+    for (int i = idx; i < virus.size(); i++) {
+        if (check[i]) {
+            continue;
+        }
+        
+        check[i] = 1;
+        DFS(i, count + 1);
+        check[i] = 0;
+    }
+}
+
+void BFS(queue<pair<int, int>> myqueue) {
+    while (!myqueue.empty()) {
+        int x = myqueue.front().first;
+        int y = myqueue.front().second;
+        myqueue.pop();
+        
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                continue;
+            }
+            
+            if (board[nx][ny] == 1) {
+                continue;
+            }
+            
+            if (visited[nx][ny] != -1) {
+                continue;
+            }
+            
+            visited[nx][ny] = visited[x][y] + 1;
+            myqueue.push(make_pair(nx, ny));
+        }
+    }
+    
+    int Max = -1;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (board[i][j] == 0) {
+                if (visited[i][j] != -1) {
+                    Max = max(Max, visited[i][j]);
+                }
+                
+                else {
+                    return;
+                }
+            }
+        }
+    }
+    if (Max == -1) {
+        Max = 0;
+    }
+
+    result = min(result, Max);
+}
