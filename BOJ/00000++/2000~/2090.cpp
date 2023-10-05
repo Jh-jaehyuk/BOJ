41f3b3f484e8528ccb5e8dbd8fa44f273670d2b9
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-10-05 20:31:19 +0900

    https://www.acmicpc.net/problem/2090 조화평균

--- BOJ/00000++/2000~/2090.cpp
+++ BOJ/00000++/2000~/2090.cpp
@@ -5,4 +5,45 @@
 //  Created by J213h on 2023/10/05.
 //
 
-#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static int A[10];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N;
+    cin >> N;
+    
+    if (N == 1) {
+        int x;
+        cin >> x;
+        cout << x << '/' << 1 << "\n";
+        return 0;
+    }
+    
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    
+    long long g = gcd(A[0], A[1]);
+    long long l = (A[0] * A[1]) / g;
+    
+    for (int i = 2; i < N; i++) {
+        g = gcd(l, 1LL * A[i]);
+        l = (l * (1LL * A[i])) / g;
+    }
+    
+    long long x = 0;
+    
+    for (int i = 0; i < N; i++) {
+        x += l / gcd(l, 1LL * A[i]);
+    }
+    
+    cout << l / gcd(l, x) << '/' << x / gcd(l, x) << "\n";
+    
+    return 0;
+}
