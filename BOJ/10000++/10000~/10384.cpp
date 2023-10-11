192805e42c1588493d72da9f5b0f1fc8a8103c7f
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-10-11 19:25:05 +0900

    https://www.acmicpc.net/problem/10384 팬그램

--- BOJ/10000++/10000~/10384.cpp
+++ BOJ/10000++/10000~/10384.cpp
@@ -5,4 +5,63 @@
 //  Created by J213h on 10/11/23.
 //
 
-#include "10384.hpp"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static int check[26];
+static string S[4] = { "Not a pangram", "Pangram!", "Double pangram!!", "Triple pangram!!!" };
+
+void Solve();
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int T;
+    cin >> T;
+    cin.ignore();
+    
+    for (int t = 1; t <= T; t++) {
+        cout << "Case " << t << ": ";
+        Solve();
+    }
+    
+    return 0;
+}
+
+void Solve() {
+    fill(check, check + 26, 0);
+    string s;
+    getline(cin, s);
+    
+    for (char& c : s) {
+        c = toupper(c);
+    }
+    
+    for (char c : s) {
+        if (c < 65 || c > 90) {
+            continue;
+        }
+        check[c - 'A']++;
+    }
+
+    int flag = 4;
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 0; j < 26; j++) {
+            if (check[j] < i) {
+                flag = i - 1;
+                break;
+            }
+        }
+        if (flag != 4) {
+            break;
+        }
+    }
+    
+    if (flag == 4) {
+        flag = 3;
+    }
+    
+    cout << S[flag] << "\n";
+}
