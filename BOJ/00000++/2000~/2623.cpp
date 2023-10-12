798459ddf9b8e2776427edb323763b6b688e434c
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-10-12 18:46:43 +0900

    NOT SOLVED!

---
+++ BOJ/00000++/2000~/2623.cpp
@@ -0,0 +1,8 @@
+//
+//  2623.cpp
+//  BOJ
+//
+//  Created by J213h on 10/12/23.
+//
+
+#include <stdio.h>
--- BOJ/00000++/5000~/5373.cpp
+++ BOJ/00000++/5000~/5373.cpp
@@ -5,4 +5,30 @@
 //  Created by J213h on 10/12/23.
 //
 
-#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static char C[6] = { 'w', 'y', 'r', 'o', 'g', 'b' };
+static int board[3][3][3];
+
+void init(int A[3][3][3]);
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    
+    
+    return 0;
+}
+
+void init(int A[3][3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                
+            }
+        }
+    }
+}
