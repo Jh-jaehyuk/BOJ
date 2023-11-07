757fe8bc321d4c2a9d70d246c7faa5145a9be1ba
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-11-07 19:29:19 +0900

    현수막 걸기 https://www.acmicpc.net/problem/30459

---
+++ BOJ/30000++/30000~/30459.py
@@ -0,0 +1,32 @@
+import sys
+
+N, M, R = map(int, sys.stdin.readline().split())
+A = list(map(int, sys.stdin.readline().split()))
+B = list(map(int, sys.stdin.readline().split()))
+
+B.sort()
+
+arr = set()
+for i in range(N):
+    for j in range(N):
+        if i == j:
+            continue
+        arr.add(abs(A[i] - A[j]))
+
+Max = -1
+
+for i in arr:
+    left, right = 0, M - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        tmp = i * B[mid] / 2
+
+        if tmp > R:
+            right = mid - 1
+
+        else:
+            left = mid + 1
+            Max = max(Max, tmp)
+
+print(Max)
