d623166013ca377a7f63fdf2d0b2d52f5f79d84f
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-11-07 19:10:19 +0900

    Factorial Frequencies https://www.acmicpc.net/problem/6479

--- BOJ/00000++/6000~/6479.py
+++ BOJ/00000++/6000~/6479.py
@@ -1,1 +1,55 @@
-print('Hello World!')
+import sys
+
+
+def solve(n):
+    tmp = 1
+    result = [0] * 10
+
+    for i in range(1, n + 1):
+        tmp *= i
+
+    str_n = str(tmp)
+
+    for i in range(len(str_n)):
+        result[int(str_n[i])] += 1
+
+    print(f'{n}! --')
+    print(' ' * 3, end='')
+    for i in range(5):
+        print(f'({i})', end='')
+        if result[i] < 10:
+            print(' ' * 4, end='')
+        elif result[i] < 100:
+            print(' ' * 3, end='')
+        else:
+            print(' ' * 2, end='')
+        print(f'{result[i]}', end='')
+        if i == 4:
+            print(' ')
+        else:
+            print(' ' * 4, end='')
+
+    print(' ' * 3, end='')
+    for i in range(5, 10):
+        print(f'({i})', end='')
+        if result[i] < 10:
+            print(' ' * 4, end='')
+        elif result[i] < 100:
+            print(' ' * 3, end='')
+        else:
+            print(' ' * 2, end='')
+        print(f'{result[i]}', end='')
+        if i == 9:
+            print(' ')
+        else:
+            print(' ' * 4, end='')
+
+
+while True:
+    n = int(sys.stdin.readline())
+    _ = sys.stdin.readline()
+
+    if n == 0:
+        break
+
+    solve(n)
