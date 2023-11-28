6e3769bef8ba80fe4005295ad7d8853720ac51d4
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-11-28 19:14:11 +0900

    플로이드에 오타가? https://www.acmicpc.net/problem/13314

--- BOJ/10000++/13000~/13314.py
+++ BOJ/10000++/13000~/13314.py
@@ -1,11 +1,8 @@
-f = open("output.txt", "w")
-
-f.write("100\n")
+print("100")
 data = [["10000"] * 99 + ["1"] for _ in range(99)]
 data.append(["1"] * 100)
 for i in range(100):
     data[i][i] = "0"
 
 for i in data:
-    f.write(" ".join(i) + "\n")
-f.close()
+    print(" ".join(i))
