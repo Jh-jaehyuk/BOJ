36938637f4b2393c5fcea79559b3f929e3ecc08c
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2023-12-30 16:18:00 +0900

    교환 분배법칙 브루트포스 풀이 https://www.acmicpc.net/problem/31000

---
+++ BOJ/30000++/31000~/31000-2.cpp
@@ -0,0 +1,8 @@
+//
+//  31000-2.cpp
+//  BOJ
+//
+//  Created by J213h on 12/30/23.
+//
+
+#include <stdio.h>
--- BOJ/30000++/31000~/31000.cpp
+++ BOJ/30000++/31000~/31000.cpp
@@ -5,4 +5,35 @@
 //  Created by J213h on 12/27/23.
 //
 
-#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N;
+    cin >> N;
+    
+
+    int result = (N * 2 + 1) * (N * 2 + 1);
+    
+    for (int a = -N; a <= N; a++) {
+        if (a == 0) {
+            continue;
+        }
+        
+        if (a < 0) {
+            result += N + (N + a);
+        }
+        
+        else {
+            result += N + (N - a + 2);
+        }
+    }
+    
+    cout << result << "\n";
+    
+    return 0;
+}
