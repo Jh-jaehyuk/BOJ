9a1c3f78bdc5ba70d95e6035e4a3342678f12777
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2024-01-15 16:19:26 +0900

    모바일 광고 입찰 https://www.acmicpc.net/problem/31246

--- BOJ/30000++/31000~/31229.cpp
+++ BOJ/30000++/31000~/31229.cpp
@@ -5,4 +5,41 @@
 //  Created by J213h on 1/15/24.
 //
 
-#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> prime;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N;
+    cin >> N;
+    
+    for (int i = 2; i < 1000001; i++) {
+        bool flag = false;
+        for (int j = 2; j <= sqrt(i); j++) {
+            if (i % j == 0) {
+                flag = true;
+                break;
+            }
+        }
+        
+        if (prime.size() >= N) {
+            break;
+        }
+        
+        if (!flag) {
+            prime.push_back(i);
+        }
+    }
+    
+    for (int i = 0; i < N - 1; i++) {
+        cout << prime[i] << " ";
+    }
+    cout << prime[N - 1] << "\n";
+    
+    return 0;
+}
