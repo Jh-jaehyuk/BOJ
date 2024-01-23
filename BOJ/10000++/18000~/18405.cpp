5445e0883695bcdc20b360cf9f2d389d2fef4afc
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2024-01-23 19:07:42 +0900

    경쟁적 전염 https://www.acmicpc.net/problem/18405

--- BOJ/10000++/18000~/18405.cpp
+++ BOJ/10000++/18000~/18405.cpp
@@ -1,11 +1,10 @@
 //
-//  main.cpp
+//  18405.cpp
 //  BOJ
 //
-//  Created by 한재혁 on 2023/09/11.
+//  Created by J213h on 1/23/24.
 //
 
-    
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -84,3 +83,4 @@ int main() {
 
     return 0;
 }
+
