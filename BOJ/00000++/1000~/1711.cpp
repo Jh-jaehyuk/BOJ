9276682ef788e7187495a760751a3d745895b848
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2024-02-17 19:47:09 +0900

    직각삼각형 https://www.acmicpc.net/problem/1711

--- BOJ/00000++/1000~/1711.cpp
+++ BOJ/00000++/1000~/1711.cpp
@@ -1,11 +1,10 @@
 //
-//  main.cpp
+//  1711.cpp
 //  BOJ
 //
-//  Created by 한재혁 on 2023/09/11.
+//  Created by J213h on 2/17/24.
 //
 
-    
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -73,3 +72,4 @@ int main() {
 
     return 0;
 }
+
