1abce0e991b4b856e187c84151523cb2525d4409
Jh-jaehyuk <jh.jaehyuk213@gmail.com>
2024-04-25 14:57:29 +0900

    https://www.acmicpc.net/problem/1141 접두사

---
+++ BOJ/00000++/1000~/1105.cpp
@@ -0,0 +1,8 @@
+//
+//  1105.cpp
+//  BOJ
+//
+//  Created by J213h on 4/22/24.
+//
+
+#include <stdio.h>
---
+++ BOJ/00000++/1000~/1446.cpp
@@ -0,0 +1,8 @@
+//
+//  1446.cpp
+//  BOJ
+//
+//  Created by J213h on 4/25/24.
+//
+
+#include <stdio.h>
