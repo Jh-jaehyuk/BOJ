cbd930e11b705e901eb8a5a9e9d342fb45172ce8
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-09-17 15:30:50 +0900

    겹치는 선분 https://www.acmicpc.net/problem/1689

--- BOJ/main.cpp
+++ BOJ/main.cpp
@@ -2,101 +2,33 @@
 
 using namespace std;
 
-int K, M;
-int cnt[10];
-vector<int> prime;
-int ans;
-set<int> num;
-
-void DFS(string now, int length) {
-    if (length == K) {
-        num.insert(stoi(now));
-        return;
-    }
-    
-    for (int i = 0; i < 10; i++){
-        if (!length && !i) {
-            continue;
-        }
-        if (cnt[i]) {
-            continue;
-        }
-        cnt[i] = 1;
-        DFS(now + to_string(i), length + 1);
-        cnt[i] = 0;
-    }
-}
+pair<int, int> A[1000001];
+priority_queue<int, vector<int>, greater<int>> pq;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    cin >> K >> M;
+    int N;
+    cin >> N;
     
-    for (int i = 2; i < 100001; i++) {
-        int flag = 0;
-        for (int j = 2; j <= sqrt(i); j++) {
-            if (i % j == 0) {
-                flag = 1;
-                break;
-            }
-        }
-        if (!flag) {
-            prime.push_back(i);
-        }
+    for (int i = 0; i < N; i++) {
+        cin >> A[i].first >> A[i].second;
     }
     
-    DFS("", 0);
+    sort(A, A + N);
     
-    int ps = (int)prime.size();
-    for (int i: num) {
-        int s = 0;
-        int e = ps - 1;
-        
-        int flag1 = 0;
-        int flag2 = 0;
-        
-        while (s < e) {
-            int x = prime[s] + prime[e];
-            
-            if (x == i) {
-                flag1 = 1;
-                break;
-            }
-            else if (x < i) {
-                s++;
-            }
-            else {
-                e--;
-            }
+    int ans = 1;
+    pq.push(A[0].second);
+    
+    for (int i = 1; i < N; i++) {
+        while (!pq.empty() && pq.top() <= A[i].first) {
+            pq.pop();
         }
-        
-        int temp = i;
-        while (temp % M == 0) {
-            temp /= M;
-        }
-        
-        s = 0;
-        e = ps - 1;
-        while (s <= e) {
-            long long x = 1LL * prime[s] * prime[e];
-            
-            if (x == 1LL * temp) {
-                flag2 = 1;
-                break;
-            }
-            else if (x < 1LL * temp) {
-                s++;
-            }
-            else {
-                e--;
-            }
-        }
-        
-        if (flag1 && flag2) {
-            ans++;
-        }
+        pq.push(A[i].second);
+        ans = max(ans, (int)pq.size());
     }
+    
     cout << ans << "\n";
     
     return 0;
